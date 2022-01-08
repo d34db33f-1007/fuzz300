5eb15e3fca9f178e36e7703f378f88440b38eaaa
railway zeppelin <63560479+d34db33f-1007@users.noreply.github.com>
2022-01-08 05:42:23 +0400

    some fixes

--- utils/scraper.py
+++ utils/scraper.py
@@ -11,49 +11,49 @@ from bs4 import BeautifulSoup
 
 class Scraper():
 
-	async def get_url(session, url):
-		user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:24.0) Gecko/20100101 Firefox/24.0'}
-		try:
-			async with session.get(url, timeout=15, headers=user_agent) as resp:
-				if len(url) < 80:
-					print(url, end='\r')
-				return await resp.read()
-		except:
-			pass
+    async def get_url(session, url):
+        user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:24.0) Gecko/20100101 Firefox/24.0'}
+        try:
+            async with session.get(url, timeout=15, headers=user_agent) as resp:
+                if len(url) < 80:
+                    print(url, end='\r')
+                return await resp.read()
+        except:
+            pass
 
-	async def get_forms(urls):
-		tasks = []
-		async with aiohttp.ClientSession(trust_env=True) as session:
-			for url in urls:
-				tasks.append(asyncio.create_task(Scraper.get_url(session, url)))
-			html_list = await asyncio.gather(*tasks)
+    async def get_forms(urls):
+        tasks = []
+        async with aiohttp.ClientSession(trust_env=True) as session:
+            for url in urls:
+                tasks.append(asyncio.create_task(Scraper.get_url(session, url)))
+            html_list = await asyncio.gather(*tasks)
 
-		print('\n')
-		forms = []
-		for i, html in enumerate(html_list, start=1):
-			print(f'Trying to find forms: {i} / {len(html_list)}', end='\r')
-			try:
-				soup = BeautifulSoup(html, "html.parser")
-				forms.append(soup.find_all("form"))
-			except:
-				continue
-		return forms
+        print('\n')
+        forms = []
+        for i, html in enumerate(html_list, start=1):
+            print(f'Trying to find forms: {i} / {len(html_list)}', end='\r')
+            try:
+                soup = BeautifulSoup(html, "html.parser")
+                forms.append(soup.find_all("form"))
+            except:
+                continue
+        return forms
 
-	def form_details(form, domain):
-		# returns action, method and form controls
-		details =  f'{form.attrs.get("method", "POST")} '.upper()
-		if (ac := form.attrs.get("action")): 
-			ac = urljoin(f'https://{domain}', ac, allow_fragments=True)
-			details += ac
-			if form.attrs.get("action").startswith('http'):
-				if domain not in form.attrs.get("action"):
-					return False
-		for i, i_tag in enumerate(form.find_all("input"), start=1):
-			details += f'?{i_tag.attrs.get("name")}=' if i==1 else f'&{i_tag.attrs.get("name")}='
-			details += f'{i_tag.attrs.get("value", "FUZZ")}'
-#			if i_tag.attrs.get("type") == "submit":
-#				return False
-		return details
+    def form_details(form, domain):
+        # returns action, method and form controls
+        details =  f'{form.attrs.get("method", "POST")} '.upper()
+        if (ac := form.attrs.get("action")): 
+            ac = urljoin(f'https://{domain}', ac, allow_fragments=True)
+            details += ac
+            if form.attrs.get("action").startswith('http'):
+                if domain not in form.attrs.get("action"):
+                    return False
+        for i, i_tag in enumerate(form.find_all("input"), start=1):
+            details += f'?{i_tag.attrs.get("name")}=' if i==1 else f'&{i_tag.attrs.get("name")}='
+            details += f'{i_tag.attrs.get("value", "FUZZ")}'
+#            if i_tag.attrs.get("type") == "submit":
+#                return False
+        return details
 
 
 
