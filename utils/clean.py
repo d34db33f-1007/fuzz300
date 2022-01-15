52e7a0669dd048b4fa7d2ac6d9bbae2dc67b3c4c
railway zeppelin <63560479+d34db33f-1007@users.noreply.github.com>
2022-01-15 19:04:02 +0400

    code improvements

--- utils/clean.py
+++ utils/clean.py
@@ -6,11 +6,7 @@ import os
 class Clean():
 
     def files():
-<<<<<<< HEAD
         files = ['urls.txt', 'some_links.txt', 'results.txt', 
-=======
-        files = ['urls.txt', 'some_links.txt', 'results.txt',
->>>>>>> c610ae991c05763ca37de7d13e2bebcfe75a99e3
                  'js_webforms.txt', 'entry_points.txt']
         for file in files:
             try:
